58d36f6485e471a364377c23710fbd5d4e463270
ciprianciurea <ciprian.ciurea@gmail.com>
2018-11-12 20:07:02 +0200

    Updated samples - use function to dynamically update the central label.

--- samples/index.js
+++ samples/index.js
@@ -3,6 +3,20 @@
 var DEFAULT_COLORS1 = ['#f08700', '#f49f0a', '#efca08', '#00a6a6', '#bbdef0'];
 var DEFAULT_COLORS2 = ['#7fb7be', '#357266', '#dacc3e', '#bc2c1a', '#7d1538'];
 
+var randomScalingFactor = function() {
+	return Math.round(Math.random() * 100);
+};
+
+document.getElementById('randomizeData').addEventListener('click', function() {
+	myChart3.config.data.datasets[0].data = [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()];
+	myChart3.update();
+});
+
+var getTotal = function() {
+	var sum = myChart3.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
+	return `Total: ${sum}`;
+}
+
 // Doughnut with multiple lines of text in the center
 var ctx = document.getElementById('chart1').getContext('2d');
 new Chart(ctx, {
@@ -24,7 +38,7 @@ new Chart(ctx, {
 		title: {
 			display: true,
 			fontSize: 20,
-			text: 'Doughnut Chart with multiple lines of text in the center'
+			text: 'Multiple lines of text'
 		},
 		animation: {
 			animateScale: true,
@@ -87,7 +101,7 @@ new Chart(ctx, {
 		title: {
 			display: true,
 			fontSize: 20,
-			text: 'Doughnut Chart with one line of text in the center'
+			text: 'One line of text'
 		},
 		animation: {
 			animateScale: true,
@@ -98,6 +112,52 @@ new Chart(ctx, {
 				labels: [
 					{
 						text: 'This is one line of text',
+						font: {
+							size: '60',
+							family: 'Arial, Helvetica, sans-serif',
+							style: 'italic',
+							weight: 'bold'
+						},
+						color: '#bc2c1a'
+					}
+				]
+			}
+		}
+	}
+});
+
+// Doughnut with one line of text in the center
+ctx = document.getElementById('chart3').getContext('2d');
+var myChart3 = new Chart(ctx, {
+	type: 'doughnut',
+	data: {
+		datasets: [{
+			data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
+			backgroundColor: DEFAULT_COLORS2,
+			label: 'Dataset 1'
+		}],
+		labels: ['Item one', 'Item two', 'Item three', 'Item four']
+	},
+	options: {
+		responsive: true,
+		legend: {
+			display: false,
+			position: 'top',
+		},
+		title: {
+			display: true,
+			fontSize: 20,
+			text: 'Calculated value'
+		},
+		animation: {
+			animateScale: true,
+			animateRotate: true
+		},
+		plugins: {
+			doughnutlabel: {
+				labels: [
+					{
+						text: getTotal,
 						font: {
 							size: '60',
 							family: 'Arial, Helvetica, sans-serif',
